cb46affea6ddb3f0244eec982765d3f6ae68795f
Anastasia Dubovik <nasthe2@gmail.com>
2021-08-03 00:21:17 +0300

    Style improvements

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <ul v-if="characters.length" class="characters-list">
-      <li v-for="item in characters" :key="item.id" class="characters-list__item">{{ item.name }}</li>
+    <ul v-if="characters.length && !isLoading" class="characters-list">
+      <li v-for="item in characters" :key="item.id" class="characters-list__item" title="Узнать подробнее">{{ item.name }}</li>
     </ul>
-    <div v-else>Ни одного персонажа не загружено :(</div>
+    <div v-else-if="!isLoading">
+      <div>Произошла ошибка :(</div>
+      <div>Пожалуйста, попробуйте повторить запрос позже</div>
+    </div>
+    <div v-else>Loading...</div>
+    <button class="more-characters-btn">Показать еще</button>
   </div>
 </template>
 
@@ -15,6 +20,7 @@ export default {
   name: 'CharactersList',
   data () {
     return {
+      isLoading: true,
       characters: [],
       charactersShown: 0
     }
@@ -30,10 +36,12 @@ export default {
           data.forEach((item) => {
             this.characters.push(item);
           })
+          this.isLoading = false;
           this.charactersShown += 10;
           console.log(this.characters);
         })
         .catch((error) => {
+          this.isLoading = false;
           console.log(error);
         });
     }
@@ -42,5 +50,18 @@ export default {
 </script>
 
 <style lang="sass">
+
+.characters-list
+  display: flex
+  flex-direction: column
+  align-items: center
+  max-width: 800px
+  list-style: none
+  &__item
+    display: inline-block
+    margin-bottom: 20px
+    cursor: pointer
+.more-characters-btn
+  cursor: pointer
 
 </style>
