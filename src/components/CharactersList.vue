0a0ce66a5749f4803721ce686325f65565f38cd5
Anastasia Dubovik <nasthe2@gmail.com>
2021-08-02 22:43:35 +0300

    Marvel project initialized

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { privateKey, publicKey } from '../marvel';
+
+export default {
+  name: {
+    CharactersList
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    getCharacters () {
+      
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/marvel.js
@@ -0,0 +1,2 @@
+export const privateKey = 'acb66525a106cd849d3b7de1d2a2e72a1af200f0';
+export const publicKey = 'f45176009d104a3e5226c0810d91e505';
